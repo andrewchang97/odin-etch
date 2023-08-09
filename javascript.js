28499bb115d143cb7d21cea8896d5bd2a366b495
Andrew <andrewhoonchang97@gmail.com>
2023-08-09 01:05:38 -0700

    Adding updates before bed. Created a 16x16 grid and added prompt from a button, next time I need tochange the grid based on prompt

--- javascript.js
+++ javascript.js
@@ -3,8 +3,18 @@ for (let i = 0; i < 16; i++){
         const square = document.createElement('div');
         square.classList.add('squareInGrid')
         container.appendChild(square);
+        square.addEventListener("mouseover", func)
     }
 }
+
+function func(event){
+   event.target.style.backgroundColor = "green";
+}
+function gridPrompt(){
+    let amount = prompt("Please enter how big you want the grid to be. I.e. 10, for a 10x10 grid. (Maximum of 100)")
+    return amount;
+}
+
 // what I need to do is create a 16x16 grid of square divs, though I'm not sure how to do that.
 //perhaps I could make each div in a line? and make 16 of them? but that seems excessive
 //i understand i should probably use flexbox, space evenly or something
