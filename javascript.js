774a5502b01dedfb7329b241957a2123e5403e8c
Andrew <andrewhoonchang97@gmail.com>
2023-08-09 12:22:22 -0700

    Added my finished etch a sketch project. I managed to figure out that I just needed to use my old code for the default grid, but instead of using i=16 and increment, make i equal to the player's input and generate a grid from there

--- javascript.js
+++ javascript.js
@@ -6,15 +6,30 @@ for (let i = 0; i < 16; i++){
         square.addEventListener("mouseover", func)
     }
 }
-
 function func(event){
    event.target.style.backgroundColor = "green";
 }
 function gridPrompt(){
-    let amount = prompt("Please enter how big you want the grid to be. I.e. 10, for a 10x10 grid. (Maximum of 100)")
-    return amount;
+    playerChoice = prompt("Please enter how big you want the grid to be. I.e. 10, for a 10x10 grid. (Maximum of 100)")
+    if (playerChoice >= 1 && playerChoice <= 100){
+    generateGrid(playerChoice);
+} else {
+    alert ("Please input a number between 1 and 100.")
 }
-
+}
+function generateGrid(){
+    container.innerHTML = ""
+    for (let i = 0; i < playerChoice; i++){
+        for (let j = 0; j < playerChoice; j++){
+            const square = document.createElement('div');
+            square.classList.add('squareInGrid2')
+            container.appendChild(square);
+            square.addEventListener("mouseover", func)
+        }
+    }
+    playerPercent = 100/playerChoice;
+    document.documentElement.style.setProperty('--player-percent', playerPercent + '%');
+}
 // what I need to do is create a 16x16 grid of square divs, though I'm not sure how to do that.
 //perhaps I could make each div in a line? and make 16 of them? but that seems excessive
 //i understand i should probably use flexbox, space evenly or something
